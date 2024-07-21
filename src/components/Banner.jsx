d47c018c6e22d67043beaef6922b9d3b7cfafdc6
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-07-21 16:50:30 +0545

    fixes banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,33 +1,31 @@
 import { Box, Group, Image, Paper, Text, Title } from "@mantine/core";
-import React from "react";
+import React, { useRef } from "react";
 import { useMediaQuery } from "@mantine/hooks";
-import { motion } from "framer-motion";
+import { inView, motion } from "framer-motion";
 import illustrationIntro from "/assets/illustration-intro.svg";
 
 const Banner = () => {
   const isMobile = useMediaQuery("(max-width: 755px)");
+  const textRef = useRef(null);
+  const isTextInView = inView(textRef, { once: true });
 
-  const title = "Towards Innovation In Education";
-  const words = title.split(" ");
-
-  const container = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { opacity: 0, y: 100 },
-    visible: { opacity: 1, y: 0 },
-  };
+  const words = "Towards Innovation In Education".split(" ");
 
   const imageVariants = {
     hidden: { opacity: 0, x: 300 },
     visible: { opacity: 1, x: 0, transition: { duration: 1.0 } },
+  };
+
+  const wordVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: (i) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.3,
+        duration: 0.5,
+      },
+    }),
   };
 
   return (
@@ -35,19 +33,21 @@ const Banner = () => {
       <Paper mt={60} bg="#FDBA74" p={20}>
         <Group justify="space-between" align="top">
           <Box>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={container}
-              style={{ display: "inline-flex", flexWrap: "wrap", gap: "10px" }}
-            >
-              {words.map((word, index) => (
-                <motion.span key={index} variants={item}>
-                  <Title mt={30} size={30} maw={500}>
+            <motion.div ref={textRef}>
+              <Title maw={350}>
+                {words.map((word, index) => (
+                  <motion.span
+                    key={index}
+                    initial="hidden"
+                    animate={isTextInView ? "visible" : "hidden"}
+                    variants={wordVariants}
+                    custom={index}
+                    style={{ display: "inline-block", marginRight: "8px" }}
+                  >
                     {word}
-                  </Title>
-                </motion.span>
-              ))}
+                  </motion.span>
+                ))}
+              </Title>
             </motion.div>
             <Text mt={40} maw={400}>
               Join with us in fostering skill development and community-driven
@@ -55,11 +55,7 @@ const Banner = () => {
               digital tools. Together, let's build a brighter future.
             </Text>
           </Box>
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={imageVariants}
-          >
+          <motion.div initial="hidden" animate="visible" variants={imageVariants}>
             <Image src={illustrationIntro} alt="" />
           </motion.div>
         </Group>
